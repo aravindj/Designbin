6a1c28d5289db1b7d4fd36ab59f3b19f427fc744
Aravind <aravindkumar.leo@gmail.com>
2012-04-24 00:14:09 +0530

    Changes to designbin_cron

--- designbin_cron.py
+++ designbin_cron.py
@@ -3,13 +3,15 @@ import os, time
 from datetime import datetime, timedelta
 from conf import Config
 
-dirs = os.listdir(Config.SITE_ROOT_FOLDER + Config.MODEL_PATH)
-past = datetime.now() - timedelta(days=10)
+subdomain_folder = Config.SITE_ROOT_FOLDER + Config.MODEL_PATH
+dirs = os.listdir(subdomain_folder)
+past = datetime.now() - timedelta(days=1)
 time_diff = time.mktime(past.timetuple())
 
 for directory in dirs:
-    mtime = os.path.getmtime(Config.MODEL_PATH  + directory)
+    mtime = os.path.getmtime(  + directory)
     if mtime < time_diff:
-        os.system("chmod 777 "+ Config.MODEL_PATH + directory)
-        os.system("rm -rf " + Config.MODEL_PATH + directory)
+        os.system("chmod 777 "+ subdomain_folder + directory)
+        os.system("rm -rf " + subdomain_folder + directory)
+        print time.ctime() + ": deleted folder " + directory
 
